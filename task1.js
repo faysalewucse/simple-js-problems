93540ef9d14a2f02eb4e06e866c7f078c934557d
faysalewucse <faysal.ewucse@gmail.com>
2023-07-25 16:36:43 +0600

    task 1 done

---
+++ task1.js
@@ -0,0 +1,9 @@
+function reverseWord(word) {
+  let reversedWord = "";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedWord += word[i];
+  }
+  return reversedWord;
+}
+
+console.log(reverseWord("Hello world"));
