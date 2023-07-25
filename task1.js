46ca8c0237ff5d1ac6500617669c33a031b2360e
faysalewucse <faysal.ewucse@gmail.com>
2023-07-25 16:39:32 +0600

    task 2 done

--- task1.js
+++ task1.js
@@ -1,3 +1,8 @@
+// Task 1: Create a function that takes a string as input and returns the reversed version of the string without using the built-in reverse() method.
+
+// Example Input: "hello world"
+// Example Output: "dlrow olleh"
+
 function reverseWord(word) {
   let reversedWord = "";
   for (let i = word.length - 1; i >= 0; i--) {
